8dabff85e7e85678d00539a5afc43276b942e99b
lfsoftware13 <lfsoftware13@gmail.com>
2017-10-14 02:18:00 +0800

    add error_util.py file

---
+++ util/error_util.py
@@ -0,0 +1,23 @@
+import re
+
+
+def deal_error(content):
+    errs = []
+    lines=content.split("\n")
+    for temp in lines:
+        temp=temp.strip()
+        temps=temp.split(">")
+        if len(temps)>1:
+            line=temp[2:]
+        else:
+            line=temp
+        pattern=re.compile(r"^(.*): (fatal |)error (\w*): (.*)$")
+        match=pattern.search(line)
+        if match:
+            position=match.group(1)
+            code=match.group(3)
+            message=match.group(4)
+            err = {'line': line, 'position': position, 'code': code, 'message': message}
+            errs.append(err)
+            #print(position+"\t"+code+"\t"+message)
+    return errs
