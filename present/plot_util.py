b9933bc8a993999f5890812dc9fb58f623242c31
Panjks <panjks@163.com>
2017-10-15 16:02:45 +0800

    Finish save_as_png() and save_as_html()

---
+++ present/plot_util.py
@@ -0,0 +1,22 @@
+import plotly.plotly as py
+import plotly.offline as offline
+
+def save_as_png(figure,filename):
+    """
+    将图表保存为.png格式
+    :param figure:list
+    :param filepath:string
+    :return:
+    """
+
+    py.sign_in('Panjks-', 't59Jl2ktBmwycqKAX8uQ')
+    py.image.save_as(figure, filename=filename)
+
+def save_as_html(figure,filename):
+    """
+    将图表保存为.html格式
+    :param figure: list
+    :param filepath: string
+    :return:
+    """
+    offline.plot(figure, auto_open=False, filename=filename)
