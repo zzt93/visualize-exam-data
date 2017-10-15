da26e2fe12fe0cb8971bdd55ac24498b4fc9107f
zzt <zengzt93@gmail.com>
2017-10-15 17:39:56 +0800

    add basic structure.

--- util/utility.py
+++
@@ -1,7 +0,0 @@
-def string_to_datetime(s):
-    import datetime
-    return datetime.datetime.strptime(s, '%Y/%m/%d %H:%M:%S')
-
-
-def timedelta_milliseconds(td):
-    return td.days*86400000 + td.seconds*1000 + td.microseconds/1000
