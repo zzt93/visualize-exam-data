82543ebd6f3211d9a668437bbbe32acfd50fdce1
WaterWan <598127577@qq.com>
2017-10-16 23:39:25 +0800

    :card_file_box: Performing new table related changes.

--- backend/database/model.py
+++ backend/database/model.py
@@ -27,18 +27,23 @@ class StudentQuestionResult(BaseModel):
         primary_key = CompositeKey('qid', 'sid')
 
 
-# 1.个人整体情况图 TODO
+# 1.个人整体情况图
 # 5.整体编码时间分布
 # 6.个人每天编码时间统计
 # 10.题目调试次数统计
+# TODO there may be some problems, the model may be modified
 class Operation(BaseModel):
-    pass
+    op_type = CharField()
+    op_happen_time = TimestampField()
+    op_last_time = IntegerField()
+    student = ForeignKeyField(Student)
+
 
 # 2.编码、调试时间总体情况统计柱状图
 # 3.编码、调试时间个人情况统计柱状图
 # 4.学生每题编码、调试的平均时间比例统计、分布
 class CodeAndDebugTime(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     code_time = IntegerField()
     debug_time = IntegerField()
@@ -48,7 +53,7 @@ class CodeAndDebugTime(BaseModel):
 
 # 7.个人外来粘贴字符数统计柱状图
 class Paste(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     paste_content = CharField()
     class Meta:
@@ -57,7 +62,7 @@ class Paste(BaseModel):
 # 8.粘贴内容分类统计柱状图
 # TODO 可能要换个名字是不是。。。也可能需要直接去掉上面那个Paste
 class Paste2(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     paste_content = CharField()
     count = IntegerField()
@@ -66,23 +71,23 @@ class Paste2(BaseModel):
 
 # 9.平均编码速度分布图
 class Speed(BaseModel):
-    userid = CharField(unique=True)
+    userid = ForeignKeyField(Student)
     # speed 的单位是字符/分钟
     speed = FloatField()
 
 # 11.学生整体调试次数分布统计
 class Debug(BaseModel):
-    userid = CharField(unique=True)
+    userid = ForeignKeyField(Student)
     debug_count = IntegerField()
 
 # 12.学生得分分布柱状图
 class ScoreInTotal(BaseModel):
-    userid = CharField(unique=True)
+    userid = ForeignKeyField(Student)
     score = FloatField()
 
 # 13.学生题目得分分布柱状图
 class ScoreInProblem(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     score = FloatField()
     class Meta:
@@ -98,7 +103,7 @@ class BuildError(BaseModel):
 
 # 15.编译失败的次数分布
 class BuildFailure(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     failed_count = IntegerField()
     success_count = IntegerField()
@@ -107,7 +112,7 @@ class BuildFailure(BaseModel):
 
 # 16.每题编码时间过少的人
 class TimeTooLittle(BaseModel):
-    userid = CharField()
+    userid = ForeignKeyField(Student)
     problemid = CharField()
     user_time = FloatField()
     class Meta:
