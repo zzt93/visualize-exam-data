a4bbd405f4d9702de6c7a847ad02c2016176805d
Panjks <panjks@163.com>
2017-10-17 22:57:45 +0800

    Finish show_coding_speed()

--- present/plot_insert.py
+++ present/plot_insert.py
@@ -1,11 +1,35 @@
+import plotly.plotly
+import plotly.graph_objs as go
+import pandas as pd
 
 
-def show_coding_speed(speed_data: list):
+def show_coding_speed(speed_data):
     """
     统计学生平均编码速度（每分钟插入字符）的人数分布。可以整体评估学生的编码速度。
     横轴为编码速度（个/min），纵轴为人数
     :param speed_data: [{'userid':str, 'speed':float}]
-    :return:
+    :return:figure
     """
-    pass
-
+    df = pd.DataFrame(speed_data)
+    df = df.set_index(['userid'])
+    # df = df.groupby(['userid'])['speed'].sum()
+    print (df)
+    max_speed = 100
+    trace = go.Histogram(
+        x=df['speed'],
+        histnorm='count',
+        name='coding speed',
+        xbins=dict(start=0, end=max_speed, size=5),
+        marker=dict(
+            color='rgb(0,162,232)',
+        )
+    )
+    data = [trace]
+    layout = go.Layout(
+        title='学生平均编码速度分布',
+        xaxis=dict(title='个/min'),
+        yaxis=dict(title='count'),
+        barmode='Stack',
+    )
+    figure = go.Figure(data=data, layout=layout)
+    return figure
