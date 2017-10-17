1de7408ef38869ff2a921aada8385805e2fea6d8
Panjks <panjks@163.com>
2017-10-17 19:23:15 +0800

    Finish show_time_less()

--- present/plot_check.py
+++ present/plot_check.py
@@ -4,6 +4,13 @@ def show_time_less(user_list: list):
     """
     每题编码时间过少的人。以平均时间*20%为阈值，当时间比此时间还少时，认为该学生没有认真完成题目
     :param user_list: [{'userid':str, 'problemid':str, 'user_time':float, 'mean_time':float}]
-    :return:
+    :return:list
     """
-    pass
+
+    time_less_student = []
+    for student in user_list:
+        if student['user_time'] < student['mean_time']*0.2:
+            time_less_student.append(student)
+
+    return time_less_student
+
