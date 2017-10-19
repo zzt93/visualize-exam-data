92df1fefede26964b208d3d701ed7202c90c7639
WaterWan <598127577@qq.com>
2017-10-20 00:31:00 +0800

    :construction: finish all interface

--- backend/interface/controller.py
+++ backend/interface/controller.py
@@ -3,6 +3,7 @@ from backend.database.model import *
 from backend.preprocess.pre_process import merge_log_score
 from backend.util.config import EID
 from backend.util.mysql_connector import MysqlConnector
+from backend.util.constant import OperatorType
 
 
 ### *1. 个人整体情况图：
@@ -59,44 +60,61 @@ def get_time_div_total():
             total_debug_time += cdt.debug_time
         if cnt == 0:
             cnt = 1
-        data.append({'question_id': qid.question_id, 'debug_time': total_debug_time / cnt, 'code_time': total_code_time / cnt})
+        data.append(
+            {'question_id': qid.question_id, 'debug_time': total_debug_time / cnt, 'code_time': total_code_time / cnt})
     return data
 
-# TODO
+
 ###  6. 个人每天编码时间统计:
-
-
+def get_work_time_personal():
+    result = []
+    for stu in Student.select():
+        day_op = {}
+        for op in Operation.select().where(Operation.student_id == stu.student_id):
+            day = op.op_happen_time.date()
+            entry = {'op_type': op.op_type, 'op_happen_time': op.op_happen_time, 'op_last_time': op.op_last_time}
+            if day in day_op:
+                day_op[day].append(entry)
+            else:
+                day_op[day] = [entry]
+            for key in day_op:
+                record = {'data': day_op[key], 'student_id': stu.student_id, 'dayid': key}
+                result.append(record)
+    return result
 
 ### 5. 整体编码时间分布:
-
-# TODO
 def get_work_time():
-    # 忽略部分实现细节
-    # data 定义格式见1
-    # return data
-    pass
+    result = []
+    for stu in Student.select():
+        day_op = {}
+        for op in Operation.select().where(Operation.student_id == stu.student_id):
+            day = op.op_happen_time.date()
+            entry = {'op_type': op.op_type, 'op_happen_time': op.op_happen_time, 'op_last_time': op.op_last_time}
+            if day in day_op:
+                day_op[day].append(entry)
+            else:
+                day_op[day] = [entry]
+            for key in day_op:
+                record = {'data': day_op[key], 'student_id': stu.student_id, 'dayid': key}
+                result.append(record)
+    return result
 
 
 ## 拷贝：
 
 ### 7. 个人外来粘贴字符数统计柱状图:
 
-# TODO
 def get_paste_length_personal():
-    # paste_data: List
-    # paste_data = [entry1, entry2……]
-    # student_id: string
-    # question_id: string
-    # paste_content: string
-    # entry = {'student_id': student_id_value, 'question_id': question_id_value, 'paste_content': paste_content_value}
-    # student_id: string
-    # return {'paste_data': paste_data_value, 'student_id': student_id_value}
-    pass
+    pasta_data = []
+    for p in Paste.select():
+        entry = {'student_id': p.student_id.student_id, 'question_id': p.question_id,
+                 'paste_content': p.paste_content, 'happen_time': p.happen_time}
+        pasta_data.append({'pasta_data': entry, 'student_id': p.student_id.student_id})
+    return pasta_data
 
     ### *8. 粘贴内容分类统计柱状图:
 
 
-# TODO
 def get_paste_content_classification():
     # paste_data: List
     # paste_data = [entry1, entry2,……]
@@ -106,15 +124,21 @@ def get_paste_content_classification():
     # count: int
     # entry = {'student_id': student_id_value, 'question_id': question_id_value, 'paste_class': paste_class_value, 'count': count_value}
     # student_id: string
-    # return {'paste_data': paste_data, 'user_id': user_id_value}
-    pass
-    ## 插入： speed的单位是字符 / 分钟
+    # return [{'paste_data': paste_data, 'student_id': student_id_value}]
+    pasta_data = []
+    source = Paste.raw(
+        'select student_id_id, question_id, paste_type, count(*) as cnt from paste GROUP BY student_id_id, question_id, paste_type')
+    for i in source:
+        paste_data = {'student_id': i.student_id.student_id, 'question_id': i.question_id,
+                      'paste_class': i.paste_type, 'count': i.cnt}
+        pasta_data.append({'paste_data': paste_data, 'student_id': i.student_id.student_id})
+    return pasta_data
+
+## 插入： speed的单位是字符 / 分钟
 
 
 # ### 9. 平均编码速度分布图:
 
-
-# TODO
 def get_coding_speed():
     # speed_data: List
     # speed_data = [entry1, entry2, ……]
@@ -122,7 +146,11 @@ def get_coding_speed():
     # speed: float
     # entry = {'student_id': student_id_value, 'speed': speed_value}
     # return speed_data
-    pass
+    speed_data = []
+    for sp in Speed.select():
+        entry = {'student_id': sp.student_id.student_id, 'speed': sp.speed}
+        speed_data.append(entry)
+    return speed_data
 
 
 ## 调试：
@@ -130,19 +158,19 @@ def get_coding_speed():
 ### 10. 题目调试次数统计:
 
 
-# TODO
+# TODO 这里少一个属性，Debug里没有question_id
 def get_debug_personal():
     # data定义见1
     # student_id: string
     # question_id: string
     # return {'data': data_value, 'student_id': student_id_value, 'question_id': question_id_value}
-    pass
+    result = []
+    return result
 
 
-### \*11. 学生整体调试次数分布统计:
+## *11. 学生整体调试次数分布统计:
 
 
-# TODO
 def get_debug_total():
     # debug_count_data: List
     # debug_count_data = [entry1, entry2, ……]
@@ -150,31 +178,36 @@ def get_debug_total():
     # debug_count: int
     # entry = {'student_id': student_id_value, 'debug_count': debug_count_value}
     # return debug_count_data
-    pass
+    debug_count_data = []
+    for de in Debug.select():
+        entry = {'student_id': de.student_id.student_id, 'debug_count': de.debug_count}
+        debug_count_data.append(entry)
+    return debug_count_data
 
 
 ## 得分：
 
 ### *12. 学生得分分布柱状图:
-
-
-# TODO
 def get_score():
     # score_data: List
     # score_data = [entry1, entry2, ……]
     # student_id: string
     # score: float
     # entry = {'student_id': student_id_value, 'score': score_value}
-    pass
-    # return score_data
+    score_data = []
+    for stu in Student.select():
+        total_score = 0
+        for sqr in StudentQuestionResult.select().where(stu.student_id == StudentQuestionResult.student_id):
+            total_score += sqr.score
+        entry = {'student_id': stu.student_id, 'score': total_score}
+        score_data.append(entry)
+    return score_data
 
 
 ### *13. 学生题目得分分布柱状图:
 
-
-# TODO
-def get_problem_score(eid=EID):
-    '''
+def get_problem_score():
+    """
     # score_data: List
     # score_data = [entry1, entry2, ……]
     # student_id: string
@@ -182,24 +215,19 @@ def get_problem_score(eid=EID):
     # score: float
     # entry = {'student_id': student_id_value, 'question_id': question_id_value, 'score': score_value}
     :return: score_data
-    '''
-    tmp, discard = merge_log_score(eid)
-    res = []
-    for sid, value in tmp.items():
-        for pid, s in value.items():
-            res.append({'student_id': sid, 'question_id': pid, 'score': s})
+    """
+    score_data = []
+    for sqr in StudentQuestionResult.select():
+        entry = {'student_id': sqr.student_id.student_id, 'question_id': sqr.question_id, 'score': sqr.score}
+        score_data.append(entry)
+    return score_data
 
-    return res
-
-
-# TODO 问一下第13个方法是不是要重写一下
 
 ## 编译：
 
 ### *14. 编译错误出现的次数分布:
 
 
-# TODO
 def get_build_error_count():
     # build_error_data: List
     # build_error_data = [entry1, entry2, ……]
@@ -208,13 +236,14 @@ def get_build_error_count():
     # count: int
     # entry = {'question_id': question_id_value, 'error_code': error_code_value, 'count': count_value}
     # return build_error_data
-    pass
+    build_error_data = []
+    for be in BuildError.select():
+        entry = {'question_id': be.question_id.question_id, 'error_code': be.error_code, 'count': be.count}
+        build_error_data.append(entry)
+    return build_error_data
 
 
 ### *15. 编译失败的次数分布:
-
-
-# TODO
 def get_build_failed_count():
     # build_data: List
     # build_data = [entry1, entry2, ……]
@@ -224,19 +253,16 @@ def get_build_failed_count():
     # success_count: int
     # entry = {'question_id': question_id_value, 'student_id': student_id_value, 'failed_count': failed_count_value, 'success_count': success_count_value}
     # return build_failed_data
-    pass
+    build_data = []
+    for br in BuildResult.select():
+        entry = {'question_id': br.question_id, 'student_id': br.student_id.student_id, 'failed_count': br.failed_count,
+                 'success_count': br.success_count}
+        build_data.append(entry)
+    return build_data
 
 
 ### *16. 每题编码时间过少的人:
 def get_time_less():
-    # user_list: List
-    # user_list = [entry1, entry2, ……]
-    # student_id: string
-    # question_id: string
-    # user_time: float
-    # mean_time: float
-    # entry = {'student_id': student_id_value, 'question_id': question_id_value, 'user_time': user_time_value, 'mean_time': mean_time_value}
-    # return user_list
     user_list = []
     for ques in QuestionInExam.select():
         cnt = 0
@@ -250,7 +276,8 @@ def get_time_less():
         print(mean_time, ques.question_id)
         for stu in StudentQuestionResult.select().where(StudentQuestionResult.question_id == ques.question_id):
             if stu.used_time < 0.2 * mean_time:
-                entry = {'student_id': stu.student_id.student_id, 'question_id': ques.question_id, 'used_time': stu.used_time,
+                entry = {'student_id': stu.student_id.student_id, 'question_id': ques.question_id,
+                         'used_time': stu.used_time,
                          'mean_time': mean_time}
                 user_list.append(entry)
     return user_list
@@ -279,5 +306,5 @@ def get_all_problem_id():
 if __name__ == '__main__':
     os.chdir('../../')
     # print(get__problem_score())
-    print(get_time_less())
+    print(get_work_time())
     # print(datetime.datetime.now().date())
