29849ec44f160a71160a580e6445ba03104b7505
Panjks <panjks@163.com>
2017-10-22 16:43:15 +0800

    Fix

--- present/plot_score.py
+++ present/plot_score.py
@@ -12,36 +12,42 @@ def show_score(score_data):
     """
 
     for data in score_data:
-        data['score'] = data['score']*100
+        data['score'] = data['score'] * 100
+    new_score_data = []
+    for data in score_data:
+        if data not in new_score_data:
+            new_score_data.append(data)
+
+    score_data = new_score_data
 
     score_distribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for student in score_data:
         if 0 <= student['score'] <= 10:
-            score_distribution[0] = score_distribution[0]+1
+            score_distribution[0] = score_distribution[0] + 1
         elif 11 <= student['score'] <= 20:
-            score_distribution[1] = score_distribution[1]+1
+            score_distribution[1] = score_distribution[1] + 1
         elif 21 <= student['score'] <= 30:
-            score_distribution[2] = score_distribution[2]+1
+            score_distribution[2] = score_distribution[2] + 1
         elif 31 <= student['score'] <= 40:
-            score_distribution[3] = score_distribution[3]+1
+            score_distribution[3] = score_distribution[3] + 1
         elif 41 <= student['score'] <= 50:
-            score_distribution[4] = score_distribution[4]+1
+            score_distribution[4] = score_distribution[4] + 1
         elif 51 <= student['score'] <= 60:
-            score_distribution[5] = score_distribution[5]+1
+            score_distribution[5] = score_distribution[5] + 1
         elif 61 <= student['score'] <= 70:
-            score_distribution[6] = score_distribution[6]+1
+            score_distribution[6] = score_distribution[6] + 1
         elif 71 <= student['score'] <= 80:
-            score_distribution[7] = score_distribution[7]+1
+            score_distribution[7] = score_distribution[7] + 1
         elif 81 <= student['score'] <= 90:
-            score_distribution[8] = score_distribution[8]+1
+            score_distribution[8] = score_distribution[8] + 1
         elif 91 <= student['score'] <= 100:
-            score_distribution[9] = score_distribution[9]+1
+            score_distribution[9] = score_distribution[9] + 1
 
     trace = go.Bar(
         x=['0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80',
            '81-90', '91-100'],
         y=score_distribution,
-        #text=score_distribution,
+        # text=score_distribution,
         textposition='auto',
         marker=dict(
             color='rgb(0,162,232)',
@@ -68,7 +74,11 @@ def show_problem_score(score_data: list, problemid: str):
     """
 
     for data in score_data:
-        data['score'] = data['score']*100
+        data['score'] = data['score'] * 100
+    new_score_data = []
+    for data in score_data:
+        if data not in new_score_data:
+            new_score_data.append(data)
 
     score_distribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for student in score_data:
@@ -106,7 +116,7 @@ def show_problem_score(score_data: list, problemid: str):
     )
     data = [trace]
     layout = go.Layout(
-        title='学生题目得分得分分布柱状图 -- '+str(problemid)
+        title='学生题目得分得分分布柱状图 -- ' + str(problemid)
     )
 
     figure = go.Figure(data=data, layout=layout)
@@ -122,6 +132,9 @@ def show_testcase_error(testcase_data: list, problemid: str):
     :param problemid:string
     :return:figure
     """
+    for data in testcase_data:
+            if data['testcase_id'] == '':
+                data['testcase_id'] == 'AC'
     df = pd.DataFrame(testcase_data)
     df = df.set_index('question_id')
     df.index = df.index.map(lambda x: str(x))
@@ -142,7 +155,7 @@ def show_testcase_error(testcase_data: list, problemid: str):
 
     data = [trace]
     layout = go.Layout(
-        title='题目测试用例错误情况柱状图 -- '+problemid
+        title='题目测试用例错误情况柱状图 -- ' + problemid
     )
 
     figure = go.Figure(data=data, layout=layout)
@@ -159,13 +172,18 @@ def show_problem_avgscore(score_data: list):
     """
 
     for data in score_data:
-        data['score'] = data['score']*100
+        data['score'] = data['score'] * 100
+
+    new_score_data = []
+    for data in score_data:
+        if data not in new_score_data:
+            new_score_data.append(data)
 
     df = pd.DataFrame(score_data)
     df = df.set_index('student_id')
     df = df.groupby(['question_id'])['score'].mean()
     df = df.sort_index()
-    df.index = df.index.map(lambda x: 'Q'+str(x))
+    df.index = df.index.map(lambda x: 'Q' + str(x))
     df = df.sort_values(ascending=False)
     df = df.round(1)
 
@@ -188,4 +206,3 @@ def show_problem_avgscore(score_data: list):
     # print(score_distribution)
     # plotly.offline.plot(fig)    # WEB中显示图片
     return figure
-
